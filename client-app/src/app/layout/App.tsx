c6f93bca41af19ec781cb19fc6c1fb4fc7e0d961
Swdenis <swdenis@mail.ru>
2021-02-20 13:56:02 +0100

    Added validation error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,12 +7,16 @@ import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
 
 function App() {
   const location = useLocation()
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage}/>
       <Route
       path={'/(.+)'}
@@ -20,13 +24,15 @@ function App() {
         ()=>(
           <>
           <NavBar/>
-      <Container style={{marginTop: '7em'}}>
-      <Switch>
-      <Route path='/activities/:id' component={ActivityDetails}/>
-      <Route path='/activities' component={ActivityDashboard}/>
-      </Switch>
-      <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
-      </Container> 
+          <Container style={{marginTop: '7em'}}>
+            <Switch>
+            <Route exact path='/activities/:id' component={ActivityDetails}/>
+            <Route path='/activities' component={ActivityDashboard}/>
+            <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
+            <Route path='/errors' component={TestErrors} />
+            <Route component={NotFound} />
+            </Switch>
+          </Container> 
           </>
         )
       }
