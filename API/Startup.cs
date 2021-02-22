60cc18410aa0c07cf88345b441c94689ffb6cc92
Swdenis <swdenis@mail.ru>
2021-02-22 20:23:07 +0100

    Started with identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
             
         }
 
