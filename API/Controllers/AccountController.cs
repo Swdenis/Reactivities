bea427c1f8d288e32afd679641c436904d0ae89b
Swdenis <swdenis@mail.ru>
2021-02-23 11:57:33 +0100

    Added register user and validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -49,6 +50,42 @@ namespace API.Controllers
 
         return Unauthorized();
 
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+        {
+            return BadRequest("Email taken");
+        }
+
+        if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+        {
+            return BadRequest("Username taken");
+        }
+
+        var user = new AppUser 
+        {
+            DisplayName = registerDto.DisplayName,
+            UserName = registerDto.Username,
+            Email = registerDto.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if(result.Succeeded) 
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user),
+                Image = null,
+                Username = user.UserName
+            };
+        }
+
+        return BadRequest("Problem registering user");
     }
 }
 }
