fd5065183c1b6f61fdedca0a01e08c977547e652
Swdenis <swdenis@mail.ru>
2021-02-23 12:04:47 +0100

    Added get current user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Services;
@@ -39,13 +40,7 @@ namespace API.Controllers
 
         if (result.Succeeded)
         {
-            return new UserDto
-            {
-                DisplayName = user.DisplayName,
-                Token = _tokenService.CreateToken(user),
-                Image = null,
-                Username = user.UserName
-            };
+            return CreateUserObject(user);
         }
 
         return Unauthorized();
@@ -76,16 +71,30 @@ namespace API.Controllers
 
         if(result.Succeeded) 
         {
-            return new UserDto
-            {
+            return CreateUserObject(user);
+        }
+
+        return BadRequest("Problem registering user");
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+        return CreateUserObject(user);
+    }
+
+    private UserDto CreateUserObject(AppUser user)
+    {
+        return new UserDto
+            { 
                 DisplayName = user.DisplayName,
                 Token = _tokenService.CreateToken(user),
                 Image = null,
                 Username = user.UserName
             };
-        }
-
-        return BadRequest("Problem registering user");
     }
 }
 }
