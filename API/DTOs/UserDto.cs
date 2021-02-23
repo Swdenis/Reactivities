266c5d089a4eb858bad51a769d14cc7b45a7a7a3
Swdenis <swdenis@mail.ru>
2021-02-23 10:20:05 +0100

    Added login controller

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -5,7 +5,6 @@ namespace API.DTOs
         public string DisplayName { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
-
         public string Image { get; set; }
     }
 }
