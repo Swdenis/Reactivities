967ed3491af824130e64eefae1fce3aec4ca1a2f
Swdenis <swdenis@mail.ru>
2021-02-23 10:55:28 +0100

    Added token service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,35 +13,40 @@ namespace API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+
+        private readonly TokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager,
+        TokenService tokenService)
         {
+            _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
 
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+        if (user == null) return Unauthorized();
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if (result.Succeeded)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
-
-            if (user == null) return Unauthorized();
-
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-            
-            if(result.Succeeded) 
+            return new UserDto
             {
-                return new UserDto 
-                {
-                    DisplayName = user.DisplayName,
-                    Token="This will be a token",
-                    Image = null,
-                    Username= user.UserName
-                };
-            }
-
-            return Unauthorized();
-
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user),
+                Image = null,
+                Username = user.UserName
+            };
         }
+
+        return Unauthorized();
+
     }
+}
 }
