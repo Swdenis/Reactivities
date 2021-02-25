7b3b709d1454c3144bf3cab6639205bc47020bd8
Swdenis <swdenis@mail.ru>
2021-02-25 18:38:49 +0100

    Added backend service for attendees

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.DTOs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -11,25 +14,28 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
-            {   var activities = await _context.Activities
-            .Include(x => x.Attendees)
-            .ThenInclude(u => u.AppUser)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities
+            .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
             .ToListAsync(cancellationToken);
-                return Result<List<Activity>>.Success(activities);
+            
+            return Result<List<ActivityDto>>.Success(activities);
             }
         }
 
     }
-}
+} 
