4ec8329f63e14cdf5a427b1a9418445ed41fa78a
Swdenis <swdenis@mail.ru>
2021-02-28 16:14:12 +0100

    Added profile photos list display

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,9 +1,26 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
+import { Card, Tab,Image,Header } from 'semantic-ui-react'
+import { Profile } from '../../app/models/profile'
+interface Props{
+    profile: Profile
+}
 
-export default observer(function ProfilePhotos()
-{
+export default observer(function ProfilePhotos({profile}: Props)
+{   
     return(
-        
+        <Tab.Pane>
+            <Header icon='image' content='Photos'/>
+            <Card.Group itemsPerRow={5}>
+                {
+                    profile.photos?.map(photo => (
+                    
+                            <Card key={photo.id}>
+                                <Image src={photo.url} />
+                            </Card>
+                    ))
+                }      
+            </Card.Group>
+        </Tab.Pane>
     )
 })
