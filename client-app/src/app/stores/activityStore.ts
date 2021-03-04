3c32beb99670df11e150c81bb7c99626709a259b
Swdenis <swdenis@mail.ru>
2021-03-04 18:02:47 +0100

    Added client side flitering

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,10 +1,11 @@
-import {makeAutoObservable, runInAction} from "mobx"
+import {keys, makeAutoObservable, reaction, runInAction} from "mobx"
 import agent from "../api/agent";
 import { Activity, ActivityFormValues } from "../models/activity";
 import {format} from 'date-fns'
 import { store } from "./store";
 import { Profile } from "../models/profile";
 import { Pagination,PagingParams } from "../models/pagination";
+import { timeStamp } from "console";
 
 export default class ActivityStore{
     activityRegistry = new Map<string, Activity>()
@@ -14,9 +15,43 @@ export default class ActivityStore{
     loadingInitial: boolean = false
     pagination: Pagination | null = null
     pagingParams: PagingParams  = new PagingParams()
+    predicate = new Map().set('all',true)
 
     constructor() {
         makeAutoObservable(this)
+
+        reaction(()=> this.predicate.keys(),
+        ()=> {
+            this.pagingParams = new PagingParams()
+            this.activityRegistry.clear()
+            this.loadActivities()
+        }
+        )
+    }
+    
+    setPredicate = (predicate: string, value: string | Date) => {
+        const resetPredicate = () => {
+            this.predicate.forEach((value,key) => {
+                if (key!== 'startDate') this.predicate.delete(key)
+            })
+        }
+        switch(predicate) {
+            case 'all':
+                resetPredicate()
+                this.predicate.set('all',true)
+                break
+            case 'isGoing':
+                resetPredicate()
+                this.predicate.set('isGoing',true)
+                break
+            case 'isHost':
+                resetPredicate()
+                this.predicate.set('isHost',true)
+                break
+            case 'startDate':
+                this.predicate.delete('startDate')
+                this.predicate.set('startDate', value)
+        }
     }
 
     setPagingParams = (pagingParams: PagingParams) => {
@@ -31,6 +66,12 @@ export default class ActivityStore{
         const params = new URLSearchParams()
         params.append('pageNumber', this.pagingParams.pageNumber.toString())
         params.append('pageSize', this.pagingParams.pageSize.toString())
+        this.predicate.forEach((value,key) => {if (key === 'startDate') {
+            params.append(key, (value as Date).toISOString())
+        } else {
+            params.append(key, value)
+        }
+        })
         return params
     }
 
