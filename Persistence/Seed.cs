16ab2c60fecbd7c9e4e30af671e137c0d5f516fc
Swdenis <swdenis@mail.ru>
2021-03-05 19:47:30 +0100

    new build

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -82,31 +82,10 @@ namespace Persistence
                             },
                         }
                     },
+                    
                     new Activity
                     {
                         Title = "Future Activity 1",
-                        Date = DateTime.Now.AddMonths(1),
-                        Description = "Activity 1 month in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "Wembly Stadium",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = true
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 2",
                         Date = DateTime.Now.AddMonths(2),
                         Description = "Activity 2 months in future",
                         Category = "food",
@@ -128,7 +107,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 3",
+                        Title = "Future Activity 2",
                         Date = DateTime.Now.AddMonths(3),
                         Description = "Activity 3 months in future",
                         Category = "drinks",
@@ -150,7 +129,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 4",
+                        Title = "Future Activity 3",
                         Date = DateTime.Now.AddMonths(4),
                         Description = "Activity 4 months in future",
                         Category = "culture",
@@ -167,7 +146,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 5",
+                        Title = "Future Activity 4",
                         Date = DateTime.Now.AddMonths(5),
                         Description = "Activity 5 months in future",
                         Category = "drinks",
@@ -189,7 +168,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 6",
+                        Title = "Future Activity 5",
                         Date = DateTime.Now.AddMonths(6),
                         Description = "Activity 6 months in future",
                         Category = "music",
@@ -211,7 +190,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 7",
+                        Title = "Future Activity 6",
                         Date = DateTime.Now.AddMonths(7),
                         Description = "Activity 7 months in future",
                         Category = "travel",
@@ -233,7 +212,7 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 8",
+                        Title = "Future Activity 7",
                         Date = DateTime.Now.AddMonths(8),
                         Description = "Activity 8 months in future",
                         Category = "drinks",
