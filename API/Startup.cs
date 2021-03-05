0f698ad8fddb1cf4d7d51312f5c40c3cbc0ab9d4
Swdenis <swdenis@mail.ru>
2021-03-05 16:55:28 +0100

    Security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,24 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {   app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseXContentTypeOptions();
+
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+
+            app.UseXfo(opt => opt.Deny());
+
+            app.UseCsp(opt => opt
+            .BlockAllMixedContent()
+            .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+            .FontSources(s => s.Self().CustomSources("https://fonts.googleapis.com","data:","https://fonts.gstatic.com"))
+            .FormActions(s => s.Self())
+            .FrameAncestors(s => s.Self())
+            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+            .ScriptSources(s => s.Self().CustomSources("sha256-owzsgyAQ1KpSMml98yhMEYRAhX2ReamM7BQSV798U6g="))
+            );
         
             if (env.IsDevelopment())
             {
@@ -61,6 +79,12 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 
+            } else
+            {
+                app.Use(async (context, next)=> {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
+                });
             }
 
             // app.UseHttpsRedirection();
