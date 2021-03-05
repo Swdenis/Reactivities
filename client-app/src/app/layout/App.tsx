e97caf57d03bdf650c55b5d8342938705dd6c894
Swdenis <swdenis@mail.ru>
2021-03-05 12:07:34 +0100

    Added private routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container, Modal} from 'semantic-ui-react';
+import { Container} from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
@@ -11,11 +11,11 @@ import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
-import LoginForm from '../../features/users/LoginForm';
 import { useStore } from '../stores/store';
 import LoadingComponents from './LoadingComponents';
 import ModalContainer from '../common/modals/ModalContainer';
 import ProfilePage from '../../features/profiles/ProfilePage';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   const location = useLocation()
@@ -42,13 +42,12 @@ function App() {
           <NavBar/>
           <Container style={{marginTop: '7em'}}>
             <Switch>
-            <Route exact path='/activities/:id' component={ActivityDetails}/>
-            <Route path='/activities' component={ActivityDashboard}/>
-            <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
-            <Route path='/profiles/:username' component={ProfilePage} />
-            <Route path='/errors' component={TestErrors} />
+            <PrivateRoute exact path='/activities/:id' component={ActivityDetails}/>
+            <PrivateRoute path='/activities' component={ActivityDashboard}/>
+            <PrivateRoute key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
+            <PrivateRoute path='/profiles/:username' component={ProfilePage} />
+            <PrivateRoute path='/errors' component={TestErrors} />
             <Route path='/server-error' component={ServerError} />
-            <Route path='/login' component={LoginForm} />
             <Route component={NotFound} />
             </Switch>
           </Container> 
